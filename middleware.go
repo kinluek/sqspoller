3febd7256958b572cb3e8441291ac964d12fabcc
Kin Chan <kinluek@gmail.com>
2020-01-15 14:38:06 +0000

    update Makefile and Dockerfile

--- middleware.go
+++ middleware.go
@@ -33,21 +33,3 @@ func wrapMiddleware(middleware []Middleware, handler Handler) Handler {
 	return handler
 }
 
-
-//func Context() Middleware {
-//	f := func(handler Handler) Handler {
-//		h := func(ctx context.Context, msg *MessageOutput, err error) error {
-//			select {
-//			case <-ctx.Done():
-//				return ctx.Err()
-//			default:
-//				if err := handler(ctx, msg, err); err != nil {
-//					return err
-//				}
-//				return nil
-//			}
-//		}
-//		return h
-//	}
-//	return f
-//}
