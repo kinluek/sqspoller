2ba7ecb9643506727986277c4d8ff8ea5531200a
Kin Chan <kinluek@gmail.com>
2020-01-15 00:44:00 +0000

    refactor poll function to return errors channel and add shutdown method to poller

--- sqspoller.go
+++ sqspoller.go
@@ -5,13 +5,13 @@ import (
 	"errors"
 	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/sqs"
-	"sync"
 	"time"
 )
 
 var (
 	ErrNoHandler         = errors.New("ErrNoHandler: no handler set on Poller instance")
 	ErrTimeoutNoMessages = errors.New("ErrTimeoutNoMessages: no new messages in given time frame")
+	ErrIntegrityIssue    = errors.New("ErrIntegrityIssue: unknown integrity issue")
 )
 
 // Handler is function which handles the incoming SQS
@@ -22,16 +22,16 @@ type Handler func(ctx context.Context, msgOutput *MessageOutput, err error) erro
 // the SQS client and provides a simple API for polling an SQS
 // queue.
 type Poller struct {
-	client *sqs.SQS
-
-	QueueURL string
+	client   *sqs.SQS
+	queueURL string
 
 	AllowTimeout      bool          // If set to true, the timeouts are taken into effect, else, timeouts are ignored.
 	TimeoutNoMessages time.Duration // Stop polling after the length of time since last message exceeds this value.
 	TimeoutShutdown   time.Duration // Poller will try to shutdown gracefully up to this time limit, where the poller will exit regardless of what's happening.
 	Interval          time.Duration // Time interval between each poll request.
 
-	shutdown chan struct{} // channel to send shutdown signal on
+	shutdown       chan struct{} // channel to send shutdown signal on
+	shutdownErrors chan error    // channel to send errors on shutdown.
 
 	handler         Handler
 	middleware      []Middleware
@@ -48,11 +48,13 @@ func New(sqsSvc *sqs.SQS, config sqs.ReceiveMessageInput, options ...request.Opt
 	p := Poller{
 		client: sqsSvc,
 
-		QueueURL: *config.QueueUrl,
+		queueURL: *config.QueueUrl,
 
 		AllowTimeout: false,
 
 		TimeoutShutdown: 5 * time.Second,
+		shutdown:        make(chan struct{}),
+		shutdownErrors:  make(chan error, 1),
 
 		receiveMsgInput: &config,
 		options:         options,
@@ -60,6 +62,7 @@ func New(sqsSvc *sqs.SQS, config sqs.ReceiveMessageInput, options ...request.Opt
 
 		ctx: context.Background(),
 	}
+
 	return &p
 }
 
@@ -79,12 +82,7 @@ func (p *Poller) StartPolling() error {
 
 	ctx, cancel := context.WithCancel(p.ctx)
 
-	pollingErrors := make(chan error)
-
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go p.poll(ctx, &wg, pollingErrors)
+	pollingErrors := p.poll(ctx)
 
 polling:
 	for {
@@ -100,73 +98,87 @@ polling:
 
 	}
 
-	// collect final error if one was sent during
-	// the shutdown process
-	select {
-	case err := <-pollingErrors:
+	// flush out any errors from requests handled
+	// during shutdown.
+	for err := range pollingErrors {
+		if err == context.Canceled {
+			p.shutdownErrors <- nil
+			return nil
+		}
 		if err != nil {
+			p.shutdownErrors <- err
 			return err
 		}
-	default:
 	}
 
-	// wait for poll to finish handling current event
-	// before exiting the program.
-	wg.Wait()
-
+	// this code should never be reached.
+	p.shutdownErrors <- ErrIntegrityIssue
 	return nil
 }
 
-func (p *Poller) poll(ctx context.Context, wg *sync.WaitGroup, errorChan chan<- error) {
-	defer wg.Done()
+func (p *Poller) poll(ctx context.Context) chan error {
 
-	timeout := time.After(p.TimeoutNoMessages)
+	errorChan := make(chan error)
 
-polling:
-	for {
-		var handlingMessage bool
-		//======================================================================
-		// Make message receive request
-		out, err := p.client.ReceiveMessageWithContext(ctx, p.receiveMsgInput, p.options...)
+	go func() {
 
-		if len(out.Messages) > 0 {
-			handlingMessage = true
+		timeout := time.After(p.TimeoutNoMessages)
+
+	polling:
+		for {
+			var handlingMessage bool
+			//======================================================================
+			// Make message receive request
+			out, err := p.client.ReceiveMessageWithContext(ctx, p.receiveMsgInput, p.options...)
+
+			if len(out.Messages) > 0 {
+				handlingMessage = true
+			}
+
+			handlerError := make(chan error)
+
+			//======================================================================
+			// Handler is called here
+			go func() {
+				if err := p.handler(ctx, convertMessage(out, p.client, p.queueURL), err); err != nil {
+					handlerError <- err
+					return
+				}
+				handlerError <- nil
+			}()
+
+			//======================================================================
+			// Wait for handler to finish or for shutdown signals
+			switch p.AllowTimeout {
+			case false:
+				if err := waitForSignals(ctx, handlerError, p.Interval); err != nil {
+					errorChan <- err
+					close(errorChan)
+					return
+				}
+				continue polling
+			case true:
+				if err := waitForSignalsWithTimeout(ctx, handlerError, p.Interval, handlingMessage, timeout); err != nil {
+					errorChan <- err
+					close(errorChan)
+					return
+				}
+				if handlingMessage {
+					timeout = time.After(p.TimeoutNoMessages)
+				}
+				continue polling
+			}
+
 		}
+	}()
 
-		handlerError := make(chan error)
+	return errorChan
+}
 
-		//======================================================================
-		// Handler is called here
-		go func() {
-			if err := p.handler(ctx, convertMessage(out, p.client, p.QueueURL), err); err != nil {
-				handlerError <- err
-				return
-			}
-			handlerError <- nil
-		}()
-
-		//======================================================================
-		// Wait for handler to finish
-		if !p.AllowTimeout {
-			if err := waitForSignals(ctx, handlerError, p.Interval); err != nil {
-				errorChan <- err
-				return
-			}
-			continue polling
-		}
-
-		if p.AllowTimeout {
-			if err := waitForSignalsWithTimeout(ctx, handlerError, p.Interval, handlingMessage, timeout); err != nil {
-				errorChan <- err
-				return
-			}
-			if handlingMessage {
-				timeout = time.After(p.TimeoutNoMessages)
-			}
-			continue polling
-		}
-
-	}
+// Shutdown gracefully shuts down the poller.
+func (p *Poller) Shutdown() error {
+	p.shutdown <- struct{}{}
+	return <-p.shutdownErrors
 }
 
 // Error is the frameworks custom error type.
