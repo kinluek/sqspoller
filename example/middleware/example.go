6376574e821d7e6c4a0813b2ffca531e6beebd75
Kin Chan <kinluek@gmail.com>
2020-01-22 00:17:10 +0000

    using middleware example

--- example/middleware/example.go
+++ example/middleware/example.go
@@ -1,28 +1,8 @@
-[![kinluek](https://circleci.com/gh/kinluek/sqspoller.svg?style=shield)](https://circleci.com/gh/kinluek/sqspoller)
-
-# SQS-Poller
-SQS-Poller is a simple queue polling framework, designed specifically to work with AWS SQS.
-
-## Installation 
-
-1. Install sqspoller:
-```sh
-$ go get -u github.com/kinluek/sqspoller
-```
-
-2. Import code:
-```go
-import "github.com/kinluek/sqspoller"
-```
-
-## Quick Start
-
-```go
-// example.go
 package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -38,50 +18,7 @@ func main() {
 	sess := session.Must(session.NewSession())
 	sqsClient := sqs.New(sess)
 
-	// use client to create default Poller instance.
-	poller := sqspoller.Default(sqsClient)
-
-	// supply polling parameters.
-	poller.ReceiveMessageParams(&sqs.ReceiveMessageInput{
-		MaxNumberOfMessages: aws.Int64(1),
-		QueueUrl:            aws.String("https://sqs.us-east-2.amazonaws.com/123456789012/MyQueue"),
-	})
-
-	// configure poll interval and handler timeout
-	poller.SetPollInterval(30 * time.Second)
-	poller.SetHandlerTimeout(120 * time.Second)
-
-	// supply handler to handle new messages
-	poller.Handle(func(ctx context.Context, client *sqs.SQS, msgOutput *sqspoller.MessageOutput, err error) error {
-
-		// check errors returned from polling the queue.
-		if err != nil {
-			return err
-		}
-		msg := msgOutput.Messages[0]
-
-		// do work on message
-		fmt.Println("GOT MESSAGE: ", msg)
-
-		// delete message from queue
-		if _, err := msg.Delete(); err != nil {
-			return err
-		}
-		return nil
-	})
-
-	// Run poller.
-	if err := poller.Run(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-```
-
-## Using Middleware
-
-```go
-func main() {
+	// use client to create Poller instance.
 	poller := sqspoller.New(sqsClient)
 
 	// IgnoreEmptyResponses stops empty message outputs from reaching the core handler
@@ -98,6 +35,10 @@ func main() {
 		MaxNumberOfMessages: aws.Int64(1),
 		QueueUrl:            aws.String("https://sqs.us-east-2.amazonaws.com/123456789012/MyQueue"),
 	})
+
+	// configure poll interval and handler timeout
+	poller.SetPollInterval(30 * time.Second)
+	poller.SetHandlerTimeout(120 * time.Second)
 
 	// supply handler to handle new messages
 	poller.Handle(func(ctx context.Context, client *sqs.SQS, msgOutput *sqspoller.MessageOutput, err error) error {
@@ -121,9 +62,8 @@ func main() {
 		}
 		return nil
 	})
-	
+
 	if err := poller.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
-```
