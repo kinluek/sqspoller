ef92806ed0c6dd0c20740b6767a8a194b2590a35
Kin Chan <kinluek@gmail.com>
2020-02-20 01:10:29 +0000

    remove outdated waitForError tests

--- wait_test.go
+++ wait_test.go
@@ -2,47 +2,9 @@ package sqspoller
 
 import (
 	"context"
-	"errors"
 	"testing"
 	"time"
 )
-
-func Test_waitForError(t *testing.T) {
-
-	t.Run("nil error from channel", func(t *testing.T) {
-		errChan := make(chan error)
-		go func() {
-			errChan <- nil
-		}()
-		if err := waitForError(context.Background(), errChan); err != nil {
-			t.Fatalf("expected waitForError() to return nil, got %v", err)
-		}
-	})
-
-	t.Run("non-nil error from channel", func(t *testing.T) {
-		errChan := make(chan error)
-		ErrConfirmed := errors.New("confirmed")
-		go func() {
-			errChan <- ErrConfirmed
-		}()
-		if err := waitForError(context.Background(), errChan); err != ErrConfirmed {
-			t.Fatalf("expected waitForError() to return ErrConfirmed, got %v", err)
-		}
-	})
-
-	t.Run("context cancelled error", func(t *testing.T) {
-		errChan := make(chan error)
-		ctx, cancel := context.WithCancel(context.Background())
-		go func() {
-			cancel()
-			time.Sleep(50 * time.Millisecond)
-			errChan <- nil
-		}()
-		if err := waitForError(ctx, errChan); err != context.Canceled {
-			t.Fatalf("expected waitForError() to return context.Canceled, got %v", err)
-		}
-	})
-}
 
 func Test_waitForInterval(t *testing.T) {
 
